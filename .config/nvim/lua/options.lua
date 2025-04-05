97ea1e22819887ca26bc7690f786d9c6d36e106c
Tom Coyne <tcoyne1729@gmail.com>
2025-04-05 20:56:34 +0000

    nvim

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+-- set up the system clipboard
+-- this is for WSL2
+vim.g.clipboard = {
+	name = "WslClipboard",
+	copy = {
+		["+"] = "clip.exe",
+		["*"] = "clip.exe",
+	},
+	paste = {
+		["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+	},
+	cache_enabled = true,
+}
